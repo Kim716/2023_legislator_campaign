6e0e33f4742f09e996e39cf3ae62d3c74dda216a
Kim716 <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-11-19 11:17:29 +0800

    chore: add base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+
+  base: "/2023_legislator_campaign",
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
